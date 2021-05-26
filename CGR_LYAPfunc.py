ff0e822ce440a1807afcf354d531c22ee6adf2b2
CleliaCorridori <56033280+CleliaCorridori@users.noreply.github.com>
2021-05-26 16:59:24 +0200

    Add files via upload

---
+++ CGR_LYAPfunc.py
@@ -0,0 +1,210 @@
+from numpy import *
+from scipy.optimize import curve_fit
+import random as rand
+
+# Here there is a set of functions to compute:
+# 1- the OTOC growth rate, called CGR, for the quantum kicked rotator;
+# 2- the OTOC growth rate, called CGR, for the classical kicked rotator;
+# 3- the Lyapunv exponent computed numerically for the classical kicked rotator.
+
+# 1. Quantum CGR numerically computed
+#computation of the last kick to use to obtain lambda fitting the OTOC
+def quantumCGR_num(C,kicks):
+    '''Function to compute numerically the CGR of the OTOC for each Kick
+    input:
+    -C: the values of the OTOC
+    -kicks: vector of kicks
+    output:
+    -quantum CGR'''
+    quant_CGR=zeros(len(kicks)-1)
+    for ii in range(1,len(kicks)):
+        quant_CGR[ii-1] = mean(log(C[ii,:]/C[ii-1,:]))
+    return(quant_CGR)
+
+def CGR_fit_lin(x,a):
+    ''' Function y=a to fit the quantum CGR function'''
+    return(a)
+
+def quantumCGR_fit(Kick, K, C_val):   
+    ''' Function to set the intervals for the fit of the quantum CGR and to compute the fit.
+    input:
+    -Kick: vector of kics
+    -K: kick strength
+    -C_val: values of the OTOC
+    -''' 
+    CGR_fit=zeros(len(K))
+    # intervals for the fit
+    for kk in range(len(K)):
+        if kk < 7:
+            ii=25
+            jj=-1
+        if (kk==7 or kk==8):
+            ii=16
+            jj=23
+        if (kk==9 or kk==10):
+            ii=15
+            jj=20
+        if (kk <13 and kk>=11):
+            ii=14
+            jj=18
+        if (kk <16 and kk>=13):
+            ii=11
+            jj=16
+        if (kk==16 or kk==17):
+            ii=8
+            jj=13
+        if (kk <20 and kk>=18):
+            ii=5
+            jj=12
+        if kk==20:
+            ii=5
+            jj=9
+        if kk==21:
+            ii=5
+            jj=8
+        if kk==22:
+            ii=4
+            jj=8
+        if (kk <26 and kk>=23):
+            ii=3
+            jj=7
+        if (kk <29 and kk>=25):
+            ii=3
+            jj=5
+        if (kk <33 and kk>=29):
+            ii=3
+            jj=4
+        if (kk <38 and kk>=33):
+            ii=2
+            jj=3
+        if (kk <44 and kk>=38):
+            ii=1
+            jj=2
+        if (kk <len(K) and kk>=44):
+            ii=0
+            jj=2
+
+        CGR_num=quantumCGR_num(C_val[:,kk,:],Kick) # numerical CGR
+        CGR_fit[kk], cov = curve_fit(CGR_fit_lin, Kick[ii:jj], CGR_num[ii:jj]) #gitted CGR*2
+
+    CGR_Q = CGR_fit/2
+    return(CGR_Q)
+
+############
+# 2. Classical CGR numerically computed
+
+def map_kr_CGR(x_i, p_i, dx_i, dp_i, Nkicks, K):
+    ''' Function to compute the evolution of the classical KR, it returns only the square momentum displacement.
+    input:
+    -x_i: initial position
+    -p_i: initial momentum
+    -dx_i: initial displacement for the position
+    -dp:i: initial displacement for the momentum
+    -Nkicks: number of kikcs
+    -K: kick strength
+    output:
+    final square displacement'''
+    #initialize useful quantities
+    p=zeros(Nkicks)
+    x=zeros(Nkicks)
+    dp=zeros(Nkicks)
+    dx=zeros(Nkicks)
+    # set the initial values
+    x[0]=x_i
+    p[0]=p_i
+    dx[0]=dx_i
+    dp[0]=dp_i
+    # comupte the evolution
+    for tt in range(Nkicks-1):
+        # map of the KR
+        p[tt+1]=(p[tt]+K*sin(x[tt]))%(2*pi)
+        x[tt+1]=(x[tt]+p[tt+1])%(2*pi)
+        # tangent map
+        dp[tt+1] = (K*cos(x[tt])*dx[tt]+dp[tt]) # momentum displacement
+        dx[tt+1] = (dx[tt]+dp[tt+1])            # position displacement
+
+    return(dp**2)
+
+############
+# 3. Classical LE numerically computed
+
+def map_kr(x_i, p_i, dx_i, dp_i, Nkicks, K):
+    '''Function to compute the evolution of N steps of the classical KR
+    input:
+    -x_i: initial position
+    -p_i: initial momentum
+    -dx_i: initial displacement for the position
+    -dp:i: initial displacement for the momentum
+    -Nkicks: number of kikcs
+    -K: kick strength
+    output:
+    -dx_f: final displacement for the position
+    -dp_f: final displacement for the momentum
+    -n_d: mean square value of the displacement
+    -x[-1]: final position after Nkikcs
+    -p[-1]: final momentum after Nkikcs'''
+    #initialize useful quantities
+    p=zeros(Nkicks+1)
+    x=zeros(Nkicks+1)
+    dp=zeros(Nkicks+1)
+    dx=zeros(Nkicks+1)
+    # set the initial values
+    x[0]=x_i
+    p[0]=p_i
+    dx[0]=dx_i
+    dp[0]=dp_i
+    # comupte the evolution
+    for tt in range(Nkicks):
+        p[tt+1]=(p[tt]+K*sin(x[tt]))%(2*pi)
+        x[tt+1]=(x[tt]+p[tt+1])%(2*pi)
+        # tangent map
+        dp[tt+1] = (K*cos(x[tt])*dx[tt]+dp[tt])
+        dx[tt+1] = (dx[tt]+dp[tt+1])
+    #norm of the displacement
+    n_d = (sqrt(dx[-1]**2+dp[-1]**2))
+
+    #final normalized displacements
+    dx_f = dx[-1]/n_d
+    dp_f = dp[-1]/n_d
+    return(dx_f,dp_f,n_d,x[-1],p[-1])
+
+
+def evoluz(P, x_i, p_i, dx_i, dp_i, Nkicks, K):
+    '''Function to compute the evolution of P steps (eachone made of Nsteps) for the classical KR.
+    input:
+    -x_i: initial position
+    -p_i: initial momentum
+    -dx_i: initial displacement for the position
+    -dp:i: initial displacement for the momentum
+    -Nkicks: number of kikcs
+    -K: kick strength
+    output:
+    n_d: mean square value of the displacement
+    '''
+    p= zeros(P)
+    x= zeros(P)
+    dp=zeros(P)
+    dx=zeros(P)
+    n_d=zeros(P)
+    x[0]=x_i
+    p[0]=p_i
+    n_d[0]=sqrt(dx_i**2+dp_i**2)
+    dx[0]=dx_i/n_d[0]
+    dp[0]=dp_i/n_d[0]
+    
+    for pp in range(P-1):
+        dx[pp+1], dp[pp+1], n_d[pp+1], x[pp+1], p[pp+1] = map_kr(x[pp], p[pp], dx[pp], dp[pp], Nkicks, K) 
+        
+    return(n_d)
+    
+
+def lyapFunc(P,Nkicks,nd):
+    '''Function to compute the Lyapunov exponents for the classical KR'''
+    lyap=(1/(Nkicks*P))*sum(log(nd))
+    return(lyap)
+
+    
+
+############
+# 4. Classical LE analitically computed
+# -> is omitted here, it is simply log(K/2)
--- OTOCfunc.py
+++ OTOCfunc.py
@@ -2,23 +2,27 @@ from numpy import *
 import math
 from joblib import Parallel, delayed
 
-def nextpow2(x):
-    #computes the exponent of the first power of 2 >=x
-    return 1 if x == 0 else math.ceil(math.log2(x))
-
-def save(mean_C,name):
+def save(input_info,name):
+    '''Functio to save the input in a file .txt'''
     with open(name, 'w') as out:
-        for line in mean_C:
+        for line in input_info:
             savetxt(out, line)
 
 #####################################################################################################################
-#####################################################################################################################
-#####################################################################################################################
+# Here there is a set of function to compute the OTOC for the quantum kiched rotator
 
 def OTOC(p_state,t,dp,N,K,T):
-    #Computes the OTOC on the state p_state at the time t with P(0)=dp
+    '''Function to compute the OTOC on the state p_state at the time t with P(0)=dp.
+    input: 
+    p_state: State in momentum representation
+    t: discretized time as number of kicks
+    dp: initial momentum at time t=0
+    N: number of eigenstate
+    K: kick strength
+    T: period
+    output: computed OTOC'''
 
-    #U*=Udagger -> exp sign + -> sign=1
+
     #First term: <i| P U* P^2 U P |i>
     A=sum(conj(p_state)*dp*Ut(dp**2*Ut(dp*p_state,2,t,dp,N,K,T),1,t,dp,N,K,T))
 
@@ -32,101 +36,129 @@ def OTOC(p_state,t,dp,N,K,T):
     D=-sum(conj(p_state)*dp*Ut(dp*Ut(dp*Ut(dp*Ut(p_state,2,t,dp,N,K,T),1,t,dp,N,K,T),2,t,dp,N,K,T),1,t,dp,N,K,T))
 
     otoc=(A+B+C+D)
-    return(sqrt((otoc.real)**2+(otoc.imag)**2))#otoc.real)
+    return(sqrt((otoc.real)**2+(otoc.imag)**2))
 
 #####################################################################################################################
 
 def Ut(p_state,sign,Nkicks,dp,N,K,T=2**-7):
-    #xvec definito dentro dp definito fuori 
+    ''' Function to compute the Floquet operator implementing the time evolution operator, U, and its 
+    Hermitian coniugate, U dagger, and using the Fourier transform and antitransform.
+    The computation are implemented considering the mass m=1
+    input: 
+    p_state: State in momentum representation
+    sign: if it is equal to 1 we compute U, if it is equal to 2 we compute U dagger
+    Nkicks: number of kicks
+    dp: initial momentum at time t=0
+    N: number of eigenstate
+    K: kick strength
+    T: period, fixed to 2**-7 by default
+    '''
+    #useful quantities:
     k=K/T
-  
-    # discretization x
+    # position x discretization 
     xmax=2*pi
-    dx=xmax/N
+    dx=xmax/N # step size for the position space
     xvec=arange(0,xmax,dx) #0=2pi
-    x_state=zeros([N],dtype = 'complex_')
+    x_state=zeros([N],dtype = 'complex_') # state in position representation
 
     # hamiltonian and temporal evolution
-    H0=(dp)**2/2 # mass=1
+    H0=(dp)**2/2
     V=k*cos(xvec) 
-    if sign==2:
+    if sign==2: # U
         U0=exp(-1j*H0*T)
         Uv=exp(-1j*V) 
-    else:
+    else:       # U dagger
         U0=exp(+1j*H0*T)
         Uv=exp(+1j*V)   
 
     temp_p=zeros([N],dtype = 'complex_') # Temporary states
     temp_x=zeros([N],dtype = 'complex_')
 
-    #Alterno calci a propagazione libera
-
-    if sign==1:
+    if sign==1: # U dagger
         x_state=(sqrt(N)*fft.ifft(p_state))
         
-
-        #così se voglio un kick solo lo fa
         for j in range(1,Nkicks+1):
             #Kick
             temp_x=Uv*x_state
             #Fourier
-            temp_p=(1/sqrt(N)*fft.fft(temp_x)) #La normalizzazione ad N è necessaria perchè altrimenti la trasformata non converge
+            temp_p=(1/sqrt(N)*fft.fft(temp_x)) 
             #Free evolution
             p_state=U0*temp_p
             #AntiFourier
             x_state=(sqrt(N)*fft.ifft(p_state)) 
-    if sign==2:
+    if sign==2: #U
         for j in range(1,Nkicks+1):
             #Free evolution
             temp_p=U0*p_state
             #AntiFourier
             temp_x=sqrt(N)*fft.ifft(temp_p)
-            
             #Kick
             x_state=Uv*temp_x
             #Fourier
             p_state=(1/sqrt(N)*fft.fft(x_state))
            
-
     return(p_state)
 
 #######################################################################################
 
 def mean_otoc(N,T,K,Nkicks,trials,meanOTOC=True):
-    '''function to set the useful discretizations and compute OTOC and its mean (set meanOTOC to True if you want to compute it)'''
-    trials=int(trials)
-    # discretization p
+    '''Function to set the useful discretizations and to compute OTOC and its mean for different values of K.
+    Set meanOTOC=True if you want to compute the mean of the OTOC
+    input: 
+    N: number of eigenstate
+    T: period, fixed to 2**-7 by default
+    K: kick strength
+    Nkicks: number of kicks
+    trials: number of trials to average
+    meanOTOC: if it is True the function return the mean value of the OTOC, if it is False 
+    function return the computed OTOC for each trial
+    output:
+    out: it is the the OTOC for each trial or the mean value of the OTOC, depending on the variable "meanOTOC"   
+    '''
+    # discretization of the momentum p
     dp=zeros([N])
     for i in range(0,N):
         dp[i]=i-(N/2)
 
-    # initial conditions
-    p_state=zeros(N,dtype = 'complex_')
-    sigma=4
+    # set initial conditions
+    p_state=zeros(N, dtype = 'complex_')
+    sigma=4 # as in the paper, it is fixed
     # OTOC computation
     C=zeros([Nkicks,len(K),trials])
-    #computes slightly different inital states in order to compute the mean OTOC 
+    # Compute different inital state for each trial: different point in the phase space
     for ii in range(trials):
         #random initial states
         p0 = random.uniform(-pi,pi,N)
-        p0=0
-        p_state=(1/sqrt(2*pi*sigma**2*N**2))*exp(-((p0-dp)**2)/(2*sigma**2)) # gaussian
+        p_state=(1/sqrt(2*pi*sigma**2*N**2))*exp(-((p0-dp)**2)/(2*sigma**2)) # gaussian as in the paper
         
+        # Comupute the OTOC for different values of K, optimization of the code parallelizing over different number of kicks
         for jj in range(len(K)):
             C[:,jj,ii]=Parallel(n_jobs=1)(delayed(OTOC)(p_state,aa,dp,N,K[jj],T) for aa in range(1,Nkicks+1))
-        print('Trial #',ii)
+        print('Trial #',ii) # To observe the computed trials when the program is running
 
-    mean_C = abs(mean(C,2))
+    mean_C = mean(C,2) # mean of the OTOC
     if meanOTOC==True:
         out=mean_C
     else:
-        out=C
+        out=squeeze(C)
     return(out)
 
 
 def mean_otoc_heff(N,T,K,Nkicks,trials,meanOTOC=True):
-    '''function to set the useful discretizations and compute OTOC and its mean (set meanOTOC to True if you want to compute it)'''
-    trials=int(trials)
+    '''Function to set the useful discretizations and to compute OTOC and its mean for 
+    different values of h_eff(=T because here we consider h_bar=1).
+    Set meanOTOC=True if you want to compute the mean of the OTOC
+    input: 
+    N: number of eigenstate
+    T: period, fixed to 2**-7 by default
+    K: kick strength
+    Nkicks: number of kicks
+    trials: number of trials to average
+    meanOTOC: if it is True the function return the mean value of the OTOC, if it is False 
+    function return the computed OTOC for each trial
+    output:
+    out: it is the the OTOC for each trial or the mean value of the OTOC, depending on the variable "meanOTOC"   
+    '''
     # discretization p
     dp=zeros([N])
     for i in range(0,N):
@@ -154,7 +186,5 @@ def mean_otoc_heff(N,T,K,Nkicks,trials,meanOTOC=True):
     else:
         out=C
     return(out)
-################################################################
-################################################################
-################################################################
+
 
